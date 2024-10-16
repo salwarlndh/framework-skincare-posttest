390279344a553e864cbd9d5ad14dfca190818758
Salwa Arlinda Humairah <126219178+salwarlndh@users.noreply.github.com>
2024-10-16 17:23:52 +0800

    Posttest4

--- posttest/skincare/app/urls.py
+++ posttest/skincare/app/urls.py
@@ -5,5 +5,9 @@ from . import views
 urlpatterns = [
     path('about', views.about),
     path('home', views.homepage),
-    path('product', views.product),
+    path('product', views.product_index2, name='product_index2'),
+    path('product/', views.product_index, name='product_index'), # Read product
+    path('product/create/', views.product_create, name='product_create'), # Create Product
+    path('product/update/<int:product_id>/', views.product_update, name='product_update'), # Update Product
+    path('product/delete/<int:product_id>', views.product_delete, name='product_delete'), # Delete Product
 ]
--- posttest/skincare/app/views.py
+++ posttest/skincare/app/views.py
@@ -1,4 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+from .models import Product
+from django.contrib import messages
+from .forms import ProductForm
+from django.http import JsonResponse
+from django.db.models import Q
 
 def homepage(request):
     return render(request, 'homepage/home.html')
@@ -8,3 +13,74 @@ def about(request):
 
 def product(request):
     return render(request, 'homepage/product.html')
+
+# READ Product
+def product_index(request):
+    product = Product.objects.all()
+    return render(request, 'product/index.html', {'product': product})
+
+def product_index2(request):
+    product = Product.objects.all()
+    return render(request, 'homepage/product.html', {'product': product})
+
+
+# CREATE Product
+def product_create(request):
+    if request.method == 'POST':
+        form = ProductForm(request.POST)
+        if form.is_valid():
+            form.save() # Simpan data product ke database
+            messages.success(request, 'Product Created') # Pesan sukses
+            return redirect('product_index') # Redirect ke halaman index product
+    else:
+        form = ProductForm()
+    return render(request, 'product/create.html', {'form': form})
+
+# UPDATE Product
+def product_update(request, product_id):
+    product = get_object_or_404(Product, id=product_id)
+    if request.method == 'POST':
+        form = ProductForm(request.POST, instance=product)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Product Updated')
+            return redirect('product_index')
+    else:
+        form = ProductForm(instance=product)
+    return render(request, 'product/update.html', {'form': form, 'product': product})
+
+# DELETE Product
+def product_delete(request, product_id):
+    product = get_object_or_404(Product, id=product_id)
+    product.delete()
+    messages.success(request, 'Product Deleted')
+    return JsonResponse({'success': True})
+
+# Search Product
+def product_index(request):
+    query = request.GET.get('q')
+    product = Product.objects.all()
+    if query:
+        product = Product.objects.filter(
+            Q(name__icontains=query) |
+            Q(description__icontains=query) |
+            Q(price__icontains=query) |
+            Q(stock__icontains=query)
+        )
+    else:
+        product = Product.objects.all()
+    return render(request, 'product/index.html', {'product': product, 'query': query})
+
+def product_index2(request):
+    query = request.GET.get('q')
+    product = Product.objects.all()
+    if query:
+        product = Product.objects.filter(
+            Q(name__icontains=query) |
+            Q(description__icontains=query) |
+            Q(price__icontains=query) |
+            Q(stock__icontains=query)
+        )
+    else:
+        product = Product.objects.all()
+    return render(request, 'homepage/product.html', {'product': product, 'query': query})
